7d801d5c7b0ed3511ba7164a8c3c906ff98faf52
luvitaintan <luvitaintan@student.ub.ac.id>
2023-05-18 14:09:57 +0700

    Initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,11 +6,10 @@
         <v-row>
           <v-col cols="5">
             <div style="position: relative" class="mt-16">
-              <h1 class="text-grey">Hello,</h1>
-              <h1 class="text-white">I'M Lara Elizabeth</h1>
-              <span class="text-grey">Web Designer & Developer</span><br />
+              <h1 class="text-white">Luvita Intan Cahyani</h1>
+              <span class="text-grey">UI/UX Designer and Graphic Designer</span><br />
 
-              <v-btn tile dark class="text-yellow mt-8" variant="outlined">
+              <v-btn tile dark class="color:white mt-8" variant="outlined">
                 Contact me
               </v-btn>
             </div>
@@ -48,104 +47,39 @@
               </div>
             </v-col>
             <v-col cols="12" sm="6">
-              <h5 class="mt-16">About Me</h5>
-              <div style="width: 120px">
-                <v-slider
-                  v-model="slider2"
-                  color="yellow"
-                  label="track-color"
-                ></v-slider>
-              </div>
-              <h4 class="mt-n4">I Am a Creative Web Designer</h4>
-              <h4 class="">And Developer</h4>
+              <h4 class="mt-2">ABOUT ME</h4>
               <p class="text-grey">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Aspernatur, ullam perspiciatis fugiat temporibus laboriosam
-                accusamus tempora repellendus sequi sed cum dolorum vero est.
-                Placeat dicta architecto reiciendis est veniam. Unde.
+                Hello, welcome to my digital Portfolio! My name is Luvita Intan or you just can call me Intan. 
+                I am undergraduate student majoring Information Technology at Brawijaya University. 
+                I have a great interest at design such as UI/UX or graphic design. 
+                I love to work in team because different perspectives of people can build 
+                a good ideas together. Just hit me up if you wanna ask something!
               </p>
-              <br />
-              <p class="text-grey">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Aspernatur, ullam perspiciatis fugiat temporibus laboriosam
-                accusamus tempora repellendus sequi sed cum dolorum vero est.
-                Placeat dicta architecto reiciendis est veniam. Unde.
-              </p>
-              <v-btn tile dark color="yellow" class="mt-4">
+              <v-btn tile dark color="#2B3D41" class="mt-4 text-white">
                 Download Resume
               </v-btn>
             </v-col>
           </v-row>
         </div>
       </v-col>
+    <v-col cols="12" class="mt-16" id="portfolio">
       <div class="text-center mt-4">
-        <h2>What we Do</h2>
-        <div style="width: 120px; margin: 0 auto">
-          <v-slider
-            v-model="slider2"
-            color="yellow"
-            label="track-color"
-          ></v-slider>
-        </div>
+        <h2>PORTFOLIO</h2>
       </div>
-      <v-col cols="12" class="padd" id="portfolio">
-        <div class="first" id="project">
-          <v-row>
-            <v-col cols="12">
-              <div class="child">
-                <v-btn
-                  icon="fas fa-laptop"
-                  color="#FBDF7E"
-                  class="text-white"
-                ></v-btn>
-                <h3 class="ml-3 mt-4">Web Design</h3>
-                <p class="text-grey ml-3 mt-4 text-caption">
-                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
-                  <br />consectetur adipis
-                </p>
-              </div>
-              <div class="child">
-                <v-btn
-                  icon="fas fa-mobile-alt"
-                  color="#FBDF7E"
-                  class="text-white"
-                ></v-btn>
-                <h3 class="ml-3 mt-4">App Design</h3>
-                <p class="text-grey ml-3 mt-4 text-caption">
-                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
-                  <br />consectetur adipis
-                </p>
-              </div>
-              <div class="child">
-                <v-btn
-                  icon="fas fa-camera"
-                  color="#FBDF7E"
-                  class="text-white"
-                ></v-btn>
-                <h3 class="ml-3 mt-4">Photography</h3>
-                <p class="text-grey ml-3 mt-4 text-caption">
-                  Lorem, ipsum dolor sit amet <br />consectetur adipisicing
-                  <br />consectetur adipis
-                </p>
-              </div>
-            </v-col>
-          </v-row>
-          <v-divider></v-divider>
-        </div>
-      </v-col>
+    </v-col>
       <v-col cols="12" sm="12" id="services">
         <div class="d-flex justify-center mb-6">
-          <v-btn color="#FBDF7E" class="mr-2">All</v-btn>
+          <v-btn color="#83A0A0" class="mr-2">All</v-btn>
           <v-btn class="mr-2" variant="tonal">Web Design</v-btn>
-          <v-btn class="mr-2" variant="tonal">Front Design</v-btn>
+          <v-btn class="mr-2" variant="tonal">Apps Design</v-btn>
           <v-btn class="mr-2" variant="tonal"> Photography</v-btn>
-          <v-btn variant="tonal"> Illustration</v-btn>
+
         </div>
       </v-col>
       <v-col cols="12" class="imgHover">
         <v-row class="fill-height" align="center" justify="center">
           <template v-for="(item, i) in items" :key="i">
-            <v-col cols="12" md="4">
+            <v-col cols="6" md="4">
               <v-hover v-slot="{ isHovering, props }">
                 <v-card
                   :elevation="isHovering ? 12 : 2"
@@ -159,84 +93,13 @@
           </template>
         </v-row>
       </v-col>
-      <v-col cols="12" sm="12">
-        <div class="d-flex justify-center mb-6">
-          <v-btn color="#FBDF7E" class="mt-4">Load More</v-btn>
-        </div>
-      </v-col>
-      <v-col cols="12" id="page">
-        <div class="pre">
-          <v-row>
-            <v-col cols="12" sm="4">
-              <v-card class="mx-auto" max-width="344" height="">
-                <v-img src="i10.jpg" height="200px" cover></v-img>
-
-                <v-card-title> We provide you the best </v-card-title>
-
-                <v-card-subtitle>
-                  By AAE IdeaPro | 06 Sep 2022
-                </v-card-subtitle>
-                <v-card-text>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  rem saepe sapiente deleniti, odio non laborum fuga.
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-card class="mx-auto" max-width="344" height="">
-                <v-img src="i11.jpg" height="200px" cover></v-img>
-
-                <v-card-title> We provide you the best </v-card-title>
-
-                <v-card-subtitle>
-                  By AAE IdeaPro | 06 Sep 2022
-                </v-card-subtitle>
-                <v-card-text>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  rem saepe sapiente deleniti, odio non laborum fuga.
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-card class="mx-auto" max-width="344" height="">
-                <v-img src="i12.jpg" height="200px" cover></v-img>
-
-                <v-card-title> We provide you the best </v-card-title>
-
-                <v-card-subtitle>
-                  By AAE IdeaPro | 06 Sep 2022
-                </v-card-subtitle>
-                <v-card-text>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                  rem saepe sapiente deleniti, odio non laborum fuga.
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </div>
-      </v-col>
-      <v-col cols="12" id="biog">
-        <div class="hire">
-          <v-row>
-            <v-col cols="12" sm="8">
-              <h1 class="mt-9">Hire me for your awesome project</h1>
-              <p class="text-grey">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
-                itaque, eaque molestiae deleniti, earum voluptate eos id dicta
-                at, blanditiis
-              </p>
-            </v-col>
-            <v-col cols="12" sm="4">
-              <v-btn color="#FBDF7E" class="mt-15">Hire Me</v-btn>
-            </v-col>
-          </v-row>
-        </div>
-      </v-col>
+      
+      
       <v-col cols="12" sm="12" class="px-16" id="contact">
         <v-row>
           <v-col cols="12" sm="4">
             <div class="child">
-              <h1>Contact info.</h1>
+              <h1>Contact Me</h1>
               <v-btn
                 icon="fas fa-map-marker-alt"
                 color=""
@@ -244,7 +107,7 @@
                 variant="outlined"
               ></v-btn
               ><br />
-              <span class="text-caption">Your Street SYO,yourArea Dream </span
+              <span class="text-caption">Jalan Bunga Kumis Kucing </span
               ><br />
               <v-btn
                 icon="fas fa-phone-alt"
@@ -253,8 +116,7 @@
                 variant="outlined"
               ></v-btn
               ><br />
-              <span class="text-caption">00235 - 6521 </span> <br />
-              <span class="text-caption">00235 - 6521 </span> <br />
+              <span class="text-caption">0812-1212-1313 </span> <br />
               <v-btn
                 icon="fas fa-envelope"
                 color=""
@@ -262,19 +124,11 @@
                 variant="outlined"
               ></v-btn
               ><br />
-              <span class="text-caption">aaeideapro@gmail.com </span> <br />
-              <span class="text-caption">aaeideapro@gmail.com </span> <br />
+              <span class="text-caption">luvitaintancahyani@gmail.com </span> <br />
             </div>
           </v-col>
           <v-col cols="12" sm="8">
-            <h1 class="mt-8">Send your message</h1>
-            <v-divider></v-divider>
-            <span class="text-caption"
-              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-              fugiat officia, odio eaque exercitationem libero nesciunt placeat,
-              repellat obcaecati sed tenetur! Est labore aliquam amet
-              consequatur necessitatibus fugit obcaecati facilis!</span
-            >
+            <h1 class="mt-8">Send me a message</h1>
             <v-row class="mt-10">
               <v-col cols="12" sm="6">
                 <v-text-field
@@ -296,7 +150,7 @@
               persistent-hint
               variant="outlined"
             ></v-textarea>
-            <v-btn color="#FBDF7E" class="mt-2">Submit Now</v-btn>
+            <v-btn color="#2B3D41" class="mt-2 text-white">Submit Now</v-btn>
           </v-col>
         </v-row>
       </v-col>
@@ -321,22 +175,22 @@ export default defineComponent({
 
       items: [
         {
-          img: "i3.jpg",
+          img: "i3.png",
         },
         {
-          img: "i4.jpg",
+          img: "i4.png",
         },
         {
-          img: "i5.jpg",
+          img: "i5.png",
         },
         {
-          img: "i6.jpeg",
+          img: "i6.png",
         },
         {
-          img: "i9.jpg",
+          img: "i7.png",
         },
         {
-          img: "i8.jpg",
+          img: "i8.png",
         },
       ],
     };
@@ -368,7 +222,7 @@ export default defineComponent({
   left: 0;
   height: 100%;
   width: 50%;
-  background: black;
+  background: #2B3D41;
   transform: skew(0deg, 6deg);
 }
 .head:after {
@@ -378,7 +232,7 @@ export default defineComponent({
   right: 0;
   height: 100%;
   width: 50%;
-  background: black;
+  background: #2B3D41;
   transform: skew(0deg, -6deg);
 }
 .egg {
@@ -387,7 +241,7 @@ export default defineComponent({
   margin-top: 50px;
   width: 356px;
   height: 300px;
-  background-color: #fbdf7e;
+  background-color: #83A0A0;
   border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
 }
 .first {
